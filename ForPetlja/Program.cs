dd752268c0073913bd3af6a406cfdf7bf6e30fbb
Dragan Pepic <d.pepic@hotmail.com>
2021-08-13 16:14:11 +0200

    Add project files.

---
+++ ForPetlja/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ForPetlja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int brojac = 1; brojac < 20; brojac++)
+            {
+                if (brojac % 2 == 0)
+                    continue;
+                Console.WriteLine($"Sada je {brojac}");
+                if (brojac == 15)
+                    break;
+            }
+        }
+    }
+}
